0be262ee1003d5554df92d571457eafb09c27bb7
Guilherme Padial <guilherme.padial4@gmail.com>
2025-02-26 10:57:03 -0300

    update: project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import './home.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import Logo from '../assets/logo.png';
+import People from '../assets/people.png';
+import People2 from '../assets/people2.png';
+import People3 from '../assets/people3.png';
+import People4 from '../assets/people4.png';
+import People5 from '../assets/people5.png';
+import Woman from '../assets/woman.png';
+import Woman2 from '../assets/woman2.png';
+import IslamicImage from '../assets/islamic-moon.png';
 
 export const Home = () => {
+  const images = [People, People2, People3, Woman, People4, Woman2, People5];
+
   return (
     <div className="landing-page">
+      <div className="background-image" style={{ backgroundImage: `url(${IslamicImage})` }}></div>
+
       <header className="header">
-        <h1>Mesquita Brasil</h1>
+        <img src={Logo} alt="Logo Mesquita Brasil" />
         <nav>
           <ul>
             <li>
@@ -25,21 +44,37 @@ export const Home = () => {
 
       <section id="about" className="about">
         <h2>Sobre a Mesquita Brasil</h2>
-        <p>A Mesquita Brasil é um lugar de paz e oração, aberta a todos que buscam conhecimento e espiritualidade.</p>
+        <p>
+          A primeira mesquita da América Latina fundada em 1928. Tem o intuito de propagar a religião e ensina-la com um conteúdo
+          diversificado.
+        </p>
       </section>
 
       <section id="gallery" className="gallery">
         <h2>Galeria</h2>
-        <div className="images">
-          <img src="url_da_imagem_1" alt="Imagem da Mesquita" />
-          <img src="url_da_imagem_2" alt="Interior da Mesquita" />
-          <img src="url_da_imagem_3" alt="Fieis em oração" />
-        </div>
+        <Swiper
+          modules={[Autoplay, Navigation]}
+          spaceBetween={0}
+          slidesPerView={3}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          navigation={true}
+          loop={true}
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <img src={image} alt={`Imagem ${index + 1}`} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </section>
 
       <section id="religion" className="religion">
         <h2>Sobre o Islamismo</h2>
-        <p>O Islamismo é uma religião de paz e submissão a Deus, baseada nos ensinamentos do Profeta Muhammad (SAW).</p>
+        <p>
+          O Islamismo é uma religião monoteísta que acredita em Allah como o único Deus e em Maomé (Muhammad) como seu último profeta. Seus
+          ensinamentos estão no Alcorão, o livro sagrado dos muçulmanos. A fé islâmica valoriza a oração, caridade, jejum e a união da
+          comunidade, promovendo paz, compaixão e justiça.
+        </p>
       </section>
 
       <section id="donation" className="donation">
@@ -54,4 +89,3 @@ export const Home = () => {
     </div>
   );
 };
-
