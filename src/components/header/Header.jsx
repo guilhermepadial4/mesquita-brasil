202ef7f0b6caf23d8646f1dc80b5293c409ed751
Guilherme Padial <guilherme.padial4@gmail.com>
2025-02-27 09:55:15 -0300

    update: header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,35 @@
+import { useState } from 'react';
 import Logo from '../../assets/logo.png';
-
 import './header.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 export function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
       <img src={Logo} alt="Logo Mesquita Brasil" />
-      <nav>
+      <button className="menu-toggle" onClick={toggleMenu} aria-label="Menu Toggle">
+        <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
+      </button>
+      <nav className={menuOpen ? 'nav-open' : ''}>
         <ul>
           <li>
-            <a href="#about">Sobre</a>
+            <a href="#about" onClick={toggleMenu}>Sobre</a>
           </li>
           <li>
-            <a href="#gallery">Galeria</a>
+            <a href="#gallery" onClick={toggleMenu}>Galeria</a>
           </li>
           <li>
-            <a href="#religion">A Religião</a>
+            <a href="#religion" onClick={toggleMenu}>A Religião</a>
           </li>
           <li>
-            <a href="#donation">Doações</a>
+            <a href="#donation" onClick={toggleMenu}>Doações</a>
           </li>
         </ul>
       </nav>
