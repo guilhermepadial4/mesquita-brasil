35b702b692f122585f4e09e530d593a41a235d19
Guilherme Padial <guilherme.padial4@gmail.com>
2025-03-06 13:26:17 -0300

    add: link in links

--- src/components/links/Links.jsx
+++ src/components/links/Links.jsx
@@ -7,8 +7,8 @@ export function Links() {
         <h2>Links Úteis</h2>
         <p>Acesse os outros sites da Mesquita Brasil:</p>
         <div className="links-grid">
-          <a href="#" className="link-item">
-            <h3>Escola Islâmica</h3>
+          <a href="https://www.islamica.com.br/" target="_blank" rel="noopener noreferrer" className="link-item">
+            <h3>Escola Islâmica Brasileira</h3>
             <p>Educação de qualidade com valores islâmicos.</p>
           </a>
           <a href="https://cemiterio-mesquita-brasil.vercel.app/" target="_blank" rel="noopener noreferrer" className="link-item">
